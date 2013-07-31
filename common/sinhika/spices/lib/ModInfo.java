95cdef42b7227262eec0f6f4e36c4906b6866ab5
Sinhika <sinhika@republicofnewhome.org>
2013-07-31 15:27:35 -0500

    Created Ant build files to create release jar
    Based on Pahimar's Equivalent Exchange build.xml

--- common/sinhika/spices/lib/ModInfo.java
+++ common/sinhika/spices/lib/ModInfo.java
@@ -5,6 +5,6 @@ public class ModInfo
 {
     public static final String ID = "sinhikaspices";
     public static final String NAME = "Sinhika's Spices";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "@VERSION@ (build @BUILD_NUMBER@)";
     public static final String CHANNEL = ID;
 }
